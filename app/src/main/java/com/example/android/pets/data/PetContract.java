5865042733980d5b768d8d4dea8b99d60802ecf9
Ivan Mitkov <ivan.mitkov@gmail.com>
2017-09-30 08:43:07 +0300

    Buggs fixed, i hope, adding possibility for inserting dummy data

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -11,15 +11,14 @@ public final class PetContract {
     /* Inner class that defines the table contents */
     public static class PetEntry implements BaseColumns{
 
-        public static String _ID=BaseColumns._ID;
+        public static final String _ID=BaseColumns._ID;
         public static final String TABLE_NAME = "pets";
         public static final String COLUMN_PET_NAME = "name";
         public static final String COLUMN_PET_BREED = "breed";
         public static final String COLUMN_PET_GENDER = "gender";
         public static final String COLUMN_PET_WEIGHT = "weight";
-        /**
-         * Possible values for the gender.
-         */
+
+
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -11,32 +11,34 @@ import com.example.android.pets.data.PetContract.PetEntry;
 
 public class PetDbHelper extends SQLiteOpenHelper{
 
+    public static final String LOG_TAG = PetDbHelper.class.getSimpleName();
 
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + PetEntry.TABLE_NAME;
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "shelter.db";
+    private static final String DATABASE_NAME = "shelter.db";
+
+    private static final int DATABASE_VERSION = 1;
+
 
     public PetDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-         String SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + PetEntry.TABLE_NAME + " (" +
-                        PetEntry._ID + " INTEGER PRIMARY KEY," +
-                        PetEntry.COLUMN_PET_NAME + " TEXT," +
-                        PetEntry.COLUMN_PET_BREED + " TEXT"+
-                        PetEntry.COLUMN_PET_GENDER + " INTEGER"+
-                        PetEntry.COLUMN_PET_WEIGHT +" INTEGER);";
-        db.execSQL(SQL_CREATE_ENTRIES);
+        // Create a String that contains the SQL statement to create the pets table
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
 
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(SQL_DELETE_ENTRIES);
-        onCreate(db);
+
     }
 }
