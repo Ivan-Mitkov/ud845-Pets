b7c622958c84209fe4082ca03ec8ad8e9386466d
Ivan Mitkov <ivan.mitkov@gmail.com>
2017-10-01 11:28:38 +0300

    Adding ContentProvider class called PetProvider

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,10 +8,16 @@ import android.provider.BaseColumns;
  */
 
 public final class PetContract {
+    //Uri constants
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+    public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
     private PetContract(){}
     /* Inner class that defines the table contents */
     public static class PetEntry implements BaseColumns{
-
+        //columns names
         public static final String _ID=BaseColumns._ID;
         public static final String TABLE_NAME = "pets";
         public static final String COLUMN_PET_NAME = "name";
@@ -18,9 +25,12 @@ public final class PetContract {
         public static final String COLUMN_PET_GENDER = "gender";
         public static final String COLUMN_PET_WEIGHT = "weight";
 
-
+        //gender options
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
+
+
+
     }
 }
